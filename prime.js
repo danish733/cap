cad240bd1762979e4becf3d6fe5a818083253d65
Md Danish <danishreza733@gmail.com>
2023-08-31 22:02:01 +0530

    prime code written

--- prime.js
+++ prime.js
@@ -1,2 +1,13 @@
-let num = 5
-console.log(num)
+let num = 17 
+let count=0
+    for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      count++
+    }
+  }
+ if(count==2) {
+     console.log("Yes")
+ }
+ else{
+     console.log("No")
+ }
